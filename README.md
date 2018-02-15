17a6abf73f9a7d5eb134ffd1bcda984a16d236c9
Claudia Stamfel <claudia@y7mail.com>
2018-02-15 18:27:32 +1100

    tidied css, added some tests

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 1. `npm i`
 1. `npm start`
 1. Enjoy
+
+### Testing Instructions
+1. `npm test`
+
+###View Live [right here](http://mobiusweb.com.au/spinmywheel/]
--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+//I need to write more unit tests. I know this but still trying to get my head around jest so this will keep changing as I learn more.
+
+test('expect 10 list items', () => {
+    document.addEventListener("DOMContentLoaded", function(event) {
+        expect(document.getElementsByTagName('li').length).toBe(10);});
 });
+
+test('expect numbers within the li span to match the numbers in data-value', () =>{
+    document.addEventListener("DOMContentLoaded", function(event) {
+        for (i=0; i <= document.getElementsByTagName('li').length; i++ ) {
+            
+            const dataValue = document.getElementsByTagName('li')[i].getAttribute('data-value');
+            const spanValue = document.getElementsByTagName('li')[i].childNodes[0];
+            expect(dataValue).toEqual(spanValue)
+        }
+    })
+})
+
+test ('expect a prediction on load between 1 and 10', () => {
+     document.addEventListener("DOMContentLoaded", function(event) {
+        const predict = document.getElementsByClassName('predict')[0];
+        expect(predict.innerHTML).toBeGreaterThan(0);
+        expect(predict.innerHTML).toBeLessThan(11);
+    })
+})
