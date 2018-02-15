555937a35c848f9dfdb6bd4e6cad7cb8c7492771
Claudia Stamfel <claudia@y7mail.com>
2018-02-15 17:34:01 +1100

    tidied up, added some ES6 stuff

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-//Please note, I haven't actually really learned much about react (4 years off raising kids). This project has been bootstrapped using create-react-app to begin. I wrote this in vanilla JS and have attempted to use react. I am sure there is a lot that I can change and I will do so once I learn more about it. Either way, tadaaaaaa. 
+//Please note, I haven't actually really learned much about react (4 years off raising kids). This project has been bootstrapped using create-react-app to begin. I wrote this in vanilla JS and have attempted to refactor using react. I am sure there is a lot that I can change and I will do so once I learn more about it. Either way, tadaaaaaa. 
 
-
+//The complete picture, adding all the components within the full module.
 class spinningWheel extends Component {
     render(){
         return(
@@ -16,8 +16,11 @@ class spinningWheel extends Component {
     }
 }
 
+//this is where the magic happens. I know that there must be a better way to handle this using react, especially from what I have read about state management but there is some lack of knowledge. Will be refactoring as I learn more about react and redux
+
+
 class Wheel extends Component {
-    
+	
     //set initial state
     constructor(props) {
         super(props);
@@ -25,9 +28,11 @@ class Wheel extends Component {
     	    initialPrediction: Math.floor(Math.random()*10)+1
         };
     };
-
+    
+	//most of the information I need relies on the markup being ready hence putting it all in componentDidMount(). 
+	
     componentDidMount(){
-        //variables
+        //variables. I dunno if this is "old school" but 4 years ago, we used to like to declare all our variables at the top. Attempted some ES6 with the whole const and let thing.
         const el = ReactDOM.findDOMNode(this).getElementsByClassName('wheel-circle')[0];
         const ans = document.getElementsByClassName("prevans")[0];
         const pred = document.getElementsByClassName("predict")[0];
@@ -52,7 +57,7 @@ class Wheel extends Component {
         let arrWind;
         let prediction = this.state.initialPrediction;
         
-        //work out angle
+        //work out angle. if you just get the property value of transform you are given a matrix. I went to stackoverflow for this one to work out how to convert to an angle. 
         if (tr !== "none") {
             values = tr.split('(')[1];
             values = values.split(')')[0];
@@ -62,12 +67,12 @@ class Wheel extends Component {
             angle = 0;
         }
         
-        //mouseDown
-		    el.onmousedown = (function(e) {mousedown()})
-            el.ontouchstart = (function(e) {mousedown()})
-        //MouseUp
-            el.onmouseup = (function(e) {mouseup()})
-            el.ontouchend= (function(e) {mouseup()})
+        //mouseDown or touchdown. I am unsure if this is how I should do it in react since most the examples I have seen show the event within the JSX component. Again one to refactor once I know more about react.
+		    el.onmousedown = (e => mousedown())
+            el.ontouchstart = (e => mousedown())
+        //MouseUp. Ditto above.
+            el.onmouseup = (e => mouseup())
+            el.ontouchend= (e => mouseup())
         
          //ok so here, I don't want to just use a random value on spin so we will get the difference between the time from the mousedown moment to the mouse up moment and use that for the rotate angle. I suspect handling the events this way defeats the purpose of react but I will revisit once I have time to read up more on react.
 
@@ -109,24 +114,16 @@ class Wheel extends Component {
 
             //push answer to the answer array and then display it on the page once spinning is complete
             answerArray.push(currentAnswer);
-            setTimeout(function() {
-                ans.innerHTML = answerArray;
-            }, 1000);
-
-
-            answerArray = answerArray.filter(function(n){ return n !== 0 }); 
+            setTimeout(() => ans.innerHTML = answerArray, 1000);
+            answerArray = answerArray.filter(n =>  n !== 0 ); 
 
             //We need to predict the next number as well as track our accuracy. I have decided to use a moving window to get the mode of the last 3 numbers of the array
             arrWind = answerArray.slice(-11).slice(0,-1).map(Number);
-			
-			//if we only have one answer in the array, our next prediction was set in the constructor as a random number so set the predictin as that otherwise, work out the prediction via the moving window.
-			answerArray.length === 1 ? prediction :
+            
+            //if we only have one answer in the array, our next prediction was set in the constructor as a random number so set the predictin as that otherwise, work out the prediction via the moving window.
+            answerArray.length === 1 ? prediction :
             prediction = arrWind.sort(function(a, b) {
-                return arrWind.filter(function(v) {
-                    return v === a
-                }).length - arrWind.filter(function(v) {
-                    return v === b
-                }).length
+                return arrWind.filter(v => v === a).length - arrWind.filter(v => v === b).length
             }).pop()
             
             //set the next prediction from above.
@@ -137,7 +134,7 @@ class Wheel extends Component {
             Number(currentAnswer) === prediction ? accuracy.push(1) : accuracy.push(0);
 			
             //finally I work out the accuracy percentage and display it on the page
-            var accuracyTotal = accuracy.reduce(function(a, b) {return a + b;})
+            var accuracyTotal = accuracy.reduce((a, b) =>  a + b)
             var accuracyPercentage = (accuracyTotal / accuracy.length) * 100;
 
              setTimeout(function() {
@@ -182,7 +179,7 @@ class Scores extends Component {
     render(){
         
         return(
-            <div className="Score">
+            <div className="score">
                 <h3>Recent Answers</h3>
                 <span className="prevans"></span>
                 <h3>Next Number Prediction *drumroll*</h3>
@@ -200,8 +197,10 @@ class Intro extends Component {
             <div className="intro">
                 <div className="error"></div>
                 <h1>Spinning Wheel</h1>
-                <p>If on desktop, click the wheel to spin it. Hold down your click for as long or as short as you want for different spin speed</p>
-                <p>On mobile, spin the wheel by doing using your finger to drag it (don't tap, swipe)</p>
+                <div className="instructions">
+	                <p>If on <span className="imp">desktop</span>, click the wheel to spin it. Hold down your click for as long or as short as you want for different spin speed</p>
+	                <p>On <span className="imp">mobile</span>, spin the wheel by using your finger to drag it from left to right (don't tap, swipe)</p>
+                </div>
             </div>
         )
     }
